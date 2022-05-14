86b4b5c5039e7793f96b94f0d4c168d243ed7a23
lpy <319244776@qq.com>
2022-05-15 01:34:42 +0800

    push commit

--- src/main/java/com/atguigu/test/GitTest.java
+++ src/main/java/com/atguigu/test/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("master test2!");
         System.out.println("hot-fix test!");
         System.out.println("hot-fix test2!");
+        System.out.println("push test2!");
     }
 }
