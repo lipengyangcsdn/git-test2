845de7fe816743caa02d0066e5e24214f5758c63
lipengyangcsdn <93464711+lipengyangcsdn@users.noreply.github.com>
2022-05-15 01:39:19 +0800

    Update GitTest.java

--- src/main/java/com/atguigu/test/GitTest.java
+++ src/main/java/com/atguigu/test/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("hot-fix test!");
         System.out.println("hot-fix test2!");
         System.out.println("push test2!");
+        System.out.println("王晓帆，好想你");
     }
 }
